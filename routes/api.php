a0fadb625e4423c0b57d4aa99f5f6f40775544e2
devprince <chuksechejunior@gmail.com>
2026-03-13 12:47:03 +0100

    Auth with Sanctum

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Api\ProductApiController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AuthController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -14,4 +15,10 @@ Route::get('/test', function () {
     ]);
 });
 
-Route::apiResource('products', ProductApiController::class);
+Route::middleware('auth:sanctum')->apiResource('products', ProductApiController::class);
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::post('/login', [AuthController::class, 'login']);
